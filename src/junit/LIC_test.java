1ca881d974e8ea3ae22547f01a3426b94ee87092
Arian Hanifi <counterstrike1337@hotmail.com>
2023-01-27 17:41:18 +0100

    refractor: move tests to new class for testing helper functions #47

--- src/junit/LIC_test.java
+++ src/junit/LIC_test.java
@@ -8,10 +8,4 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class LIC_test {
-    @Test
-    public void test_circumscribed_circle_radius() {
-        int[][] datapoints = {{0,3},{3,0},{-3,0}};
-        double radius = Helper_Functions.circumscribed_circle_radius(datapoints[0],datapoints[1], datapoints[2]);
-        assertTrue(radius==3.0);
-    }
 }
