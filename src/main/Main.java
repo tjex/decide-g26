89d41e819335a65d938cb9dbd19dbd30b085eb3e
Anders Blomqvist <anders@minaemail.se>
2023-01-27 16:52:33 +0100

    refactor Remade main with decide function with each step.

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,28 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        decide();
+    }
+
+    private static void decide() {
+        // Get inputs such as:
+        // - points
+        // - puv_vec
+        // - lcm_mat
+
+        //  1. Calculate the CMV
+        // CMV cmv = new CMV(data_points);
+        // boolean[] cmv_vec = cmv.getVector();
+
+        //  2. Calculate the PUM
+        // PUM pum = new PUM(some_argument, another_argument);
+        // boolean[][] pum_mat = pum.calculate_pum();
+
+        //  3. Calculate the FUV
+        // FUV fuv = new FUV();
+        // boolean[] fuv_vec = fuv.calculate_fuv(puv_vec, pum_mat)
+
+        //  4. Launch or no launch?
+        // if (fuv.is_all_true(fuv_vec)) print("YES") otherwise print("NO")
     }
 }
