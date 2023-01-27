2dea887b9336026d3490c9616b6e997197fcc401
Anders Blomqvist <anders@minaemail.se>
2023-01-27 15:39:00 +0100

    feat FUV Calculation function #32

---
+++ src/main/FUV.java
@@ -0,0 +1,29 @@
+package main;
+
+public class FUV {
+    
+    private boolean[] fuv = new boolean[15];
+
+    public FUV() {}
+
+    public boolean[] calculate_fuv(boolean[] puv, boolean[][] pum) {
+        for (int i = 0; i < fuv.length; i++) {
+            if (puv[i] == false || is_all_true(pum[i]))
+                fuv[i] = true;
+            else 
+                fuv[i] = false;
+        }
+        return fuv;
+    }
+
+    /**
+     *  Check if all values within a boolean array are set to true.
+     *  
+     *  @returns True if all boolean values are true.
+     */
+    public boolean is_all_true(boolean[] arr) {
+        if (arr.length <= 0) return false;
+        for(boolean b : arr) if(!b) return false;
+        return true;
+    }
+}
