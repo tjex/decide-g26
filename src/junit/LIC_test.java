a618618fd37ee43aeca7673e087ba77efb55549e
a-kbd <jari.derkova@gmail.com>
2023-01-28 15:57:17 +0100

    feat: lic9 done according to issue #14

--- src/junit/LIC_test.java
+++ src/junit/LIC_test.java
@@ -2,6 +2,7 @@ package junit;
 
 import main.CMV;
 import main.Helper_Functions;
+import main.Parameters;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
@@ -33,6 +34,33 @@ public class LIC_test {
 
     @Test
     public void test_lic9(){
+        Parameters.EPSILON = 0.5; //is this safe? will it set globally or just for this test?
+        //this epsilon allows angle to be < 2.64 and > 3.64
+        //this is very arbitrary since our formula calculates angles < pi
+        //how would you know that the outer angle should be used for the points?
+        Parameters.C_PTS = 2;
+        Parameters.D_PTS = 3;
+
+        int[][] datapoints1 = {{1,1}, {2,2}, {3,3}};
+        CMV cmv1 = new CMV(datapoints1);
         
+
+        int[][] datapoints2 = {{1,1}, {2,2}, {3,3}, {1,1}, {2,2}, {3,3}, {1,1}, {2,2}, {3,3}};
+        CMV cmv2 = new CMV(datapoints2);
+
+        //90deg=1.57rad angle < 3.14 - EPSILON
+        int[][] datapoints3 = {{1,1}, {0,0}, {0,0}, {1,3}, {0,0}, {0,0}, {0,0}, {3,3}}; 
+        CMV cmv3 = new CMV(datapoints3);
+
+        int[][] datapoints4 = {{0,1}, {0,0}, {0,0}, {2,3}, {0,0}, {0,0}, {0,0}, {4,4}}; 
+        CMV cmv4 = new CMV(datapoints4);
+        
+        
+        
+        assertFalse(cmv1.get_cmv_value(9)); //datapoints fewer than 5
+        assertFalse(cmv2.get_cmv_value(9)); //no such points
+        assertTrue(cmv3.get_cmv_value(9)); //points with 90 deg angle
+        assertFalse(cmv4.get_cmv_value(9)); //points with a very obtuse angle almost 180deg
+
     }
 }
