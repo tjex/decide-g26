f54329cab2b6f31f693df3c243972a227362c6f4
a-kbd <jari.derkova@gmail.com>
2023-01-28 14:39:06 +0100

    feat: added a getter function to the cmv, issue #54
    Added a getter function to the CMV class, enables us to test the
    lic-calculate functions by just accessing the cmv_vector without having
    to make the lic-calculate functions public

--- src/junit/LIC_test.java
+++ src/junit/LIC_test.java
@@ -8,6 +8,14 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class LIC_test {
+
+    @Test 
+    public void cmv_getter_test(){
+        //pretty useless test, works cause lic5-calculate is run in the constructor
+        int[][] datapoints = {{4,0},{3,0},{2,0},{1,0},{0,0}};
+        CMV cmv = new CMV(datapoints);
+        assertTrue(cmv.get_cmv_value(5));
+    }
     //LIC 5
     @Test
     public void checkIfValidGivesTrue(){
