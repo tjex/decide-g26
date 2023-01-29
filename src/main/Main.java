6a055a381298ba09ee19afe48acc9ee775956774
Anders Blomqvist <anders@minaemail.se>
2023-01-29 12:13:10 +0100

    wip: Implemented main parts for `decide` function #42
    - The parameters has to be initialized.
    - Tests for decide function needs to be created.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,29 +1,69 @@
 package main;
 
+/**
+ *  Main entry point of the Decide Missile Program.
+ * 
+ *  decide() assesses which launch conditions are relevant for the immediate situation. 
+ *  If all combinations of launch conditions are met, Decide() will issue a launch-unlock 
+ *  signal, enabling the launch of the defensive projectile. 
+ * 
+ *  @author andersblomqvist
+ *  @author a-kbd
+ *  @author coffecup25
+ *  @author darpos
+ *  @author tjex
+ */
 public class Main {
     public static void main(String[] args) {
-        decide();
+        Main launcher = new Main();
+        boolean lanch = launcher.decide();
+        if (lanch)
+            System.out.println("YES");
+        else
+            System.out.println("NO");
     }
 
-    private static void decide() {
-        // Get inputs such as:
-        // - points
-        // - puv_vec
-        // - lcm_mat
+    public Main() {
+        init_parameters();
+    }
 
+    private boolean decide() {
         //  1. Calculate the CMV
-        // CMV cmv = new CMV(data_points);
-        // boolean[] cmv_vec = cmv.getVector();
+        CMV cmv = new CMV(Parameters.DATA_POINTS);
+        boolean[] cmv_vec = getCMV(cmv);
 
         //  2. Calculate the PUM
-        // PUM pum = new PUM(some_argument, another_argument);
-        // boolean[][] pum_mat = pum.calculate_pum();
+        PUM pum = new PUM(Parameters.LCM_MAT, cmv_vec);
+        boolean[][] pum_mat = pum.calculate_pum();
 
         //  3. Calculate the FUV
-        // FUV fuv = new FUV();
-        // boolean[] fuv_vec = fuv.calculate_fuv(puv_vec, pum_mat)
+        FUV fuv = new FUV();
+        boolean[] fuv_vec = fuv.calculate_fuv(Parameters.PUV_VEC, pum_mat);
 
         //  4. Launch or no launch?
-        // if (fuv.is_all_true(fuv_vec)) print("YES") otherwise print("NO")
+        if (fuv.is_all_true(fuv_vec))
+            return true;
+        else
+            return false;
+    }
+
+    /**
+     *  Retrieve all LIC into one boolean vector.
+     * 
+     *  @param cmv CMV object
+     *  @returns boolean vector contain value for each LIC.
+     */
+    private boolean[] getCMV(CMV cmv) {
+        boolean[] vec = new boolean[15];
+        for(int i = 0; i < 15; i++)
+            vec[i] = cmv.get_cmv_value(i);
+        return vec;
+    }
+
+    /**
+     *  Sets all the parameters.
+     */
+    private void init_parameters() {
+        // TODO: set all variables in Parameter.class
     }
 }
