882efc0e42e6f35252b7e8bf134027a8f6f48da3
a-kbd <jari.derkova@gmail.com>
2023-01-31 11:03:24 +0100

    fix: added comments to some of the tests, issue #109

--- src/junit/Helper_Function_Test.java
+++ src/junit/Helper_Function_Test.java
@@ -18,6 +18,7 @@ public class Helper_Function_Test {
         assertTrue(radius==3.0);
     }
 
+    //this tests the function that returns the dot product of two vectors
     @Test
     public void test_dot_product(){
         double[] a1 = {1,2};
@@ -29,11 +30,14 @@ public class Helper_Function_Test {
         double[] a3 = {-1,-2};
         double [] b3 = {-3,-23424};
 
+        //the vectors a1 and b1 should have the scalar product 8, calculated by hand
         assertEquals(Helper_Functions.dot_product(a1, b1), 8, 0.1);
+        //similar to test above
         assertEquals(Helper_Functions.dot_product(a2, b2), -46851, 0.1);
         assertEquals(Helper_Functions.dot_product(a3, b3), 46851, 0.1);
     }
 
+    //tests whether the helper function vector_subtraction finds the correct vector
     @Test
     public void test_vector_subtraction(){
         double[] a1 = {3,2};
@@ -44,23 +48,26 @@ public class Helper_Function_Test {
         double[] b2 = {23,0};
         double[] r2 = {-26,-23423523};
 
-
+        //tests the function for the vectors above
         assertTrue(Arrays.equals(Helper_Functions.vector_subtraction(a1, b1), r1));
         assertTrue(Arrays.equals(Helper_Functions.vector_subtraction(a2, b2), r2));
         assertFalse(Arrays.equals(Helper_Functions.vector_subtraction(a2, b2), r1));
     }
 
+    //this tests whether the helper function vector_magnitude finds the vector magnitude
     @Test
     public void test_vector_magnitude(){
         double[] a1 = {12,32};
         double[] b1 = {0,0};
         double[] c1 = {-1,123};
 
+        //asserts that the vector magnitude equals the expected magnitude for the vectors above
         assertEquals(Helper_Functions.vector_magnitude(a1), 34.18, 0.01);
         assertEquals(Helper_Functions.vector_magnitude(b1), 0.0, 0.01);
         assertEquals(Helper_Functions.vector_magnitude(c1), 123.00406497347964, 0.01);
     }
 
+    //this tests whether the helper function finds the angle given three points
     @Test
     public void test_three_point_angle(){
         double[] origin = {0,0};
@@ -80,19 +87,25 @@ public class Helper_Function_Test {
         double three_point_angle = Helper_Functions.three_point_angle(ray1, vertex, ray2);
         
 
+        //checks that the angle given the points matches what is expected from the formula calculated on a calculator
         assertEquals(Helper_Functions.three_point_angle(a1, origin, b1), 0.1974, 0.01);
         assertEquals(Helper_Functions.three_point_angle(a2, origin, b2), 2.3214, 0.01);
         assertEquals(origin_angle, three_point_angle, 0.01);
         assertEquals(three_point_angle, 1.008, 0.01);
     }
 
+    //this tests whether the helper function triangle_vertex_area finds the area of 
+    //a triangle given its vertices
     @Test
     public void test_triangle_vertex_area(){
         double[] a1 = {-4,0};
         double[] b1 = {4,0};
         double[] c1 = {0,4};
 
+        //tests that the triangle above has the expected area 16
         assertEquals(Helper_Functions.triangle_vertex_area(a1, b1, c1), 16, 0.1);
+        //the dual of the test above
+        assertFalse(Helper_Functions.triangle_vertex_area(a1,b1,c1)==12);
     }
     /*
      * Test logic of quadrant evaluation from quadEvaluation().
